70df7e1f7d0412af27135e22acd24f1e072cb5e5
PoommipatWat <poommipat_wat@cmu.ac.th>
2022-01-15 20:29:11 +0700

    Uncomplete

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -14,21 +14,70 @@ void findColSum(const double *,double *,int,int);
 
 int main(){
 	srand(time(0));
-	const int N = 6, M = 8;
-	double data[N][M] = {};
-	double sum1[N] = {};
-	double sum2[M] = {};
-	double *dPtr = data[0];
-	randData(dPtr,N,M);
-	showData(dPtr,N,M);
+	// const int N = 6, M = 8;
+	// double data[N][M] = {};
+	// double sum1[N] = {};
+	// double sum2[M] = {};
+	// double *dPtr = data[0];
+	// randData(dPtr,N,M);
+	// showData(dPtr,N,M);
+
+	// cout << "---------------------------------------------\n";
+
+	// findRowSum(dPtr,sum1,N,M);
+	// showData(sum1,N,1);
 	
-	cout << "---------------------------------------------\n";
+	// cout << "---------------------------------------------\n";
 	
-	findRowSum(dPtr,sum1,N,M);
-	showData(sum1,N,1);
+	// findColSum(dPtr,sum2,N,M); 
+	// showData(sum2,1,M);
 	
-	cout << "---------------------------------------------\n";
 	
-	findColSum(dPtr,sum2,N,M); 
-	showData(sum2,1,M);
+	double a[3][2] = {1,2,3,4,5,6};
+	double b[3];
+	findRowSum(a[0],b,3,2);
+	showData(b,3,1);
+}
+
+void showData(double *dPtr, int N, int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << dPtr[i];
+        if((i+1)%(M) == 0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr, int N, int M){
+    for(int i = 0; i < N*M; i++){
+        dPtr[i] = (rand()%101)/100.00;
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum1,int N,int M){
+	int count = 0;
+	if (M == 1){
+		for (int i  = 0; i < N; i++){
+			sum1[i] = dPtr[i];
+		}
+	}
+	else{
+		for (int i  = 0; i < M*N; i++){
+			sum1[count] += dPtr[i];
+			if((i+1)%M == 0) count++;
+		}
+	}
+}
+
+void findColSum(const double *dPtr,double *sum1,int N,int M){
+	if(N == 1){
+		for (int i  = 0; i < M; i++){
+			sum1[i] = dPtr[i];
+		}
+	}
+	else{
+		for (int i  = 0; i < M*N; i++){
+			sum1[(i)%M] += dPtr[i];
+		}
+	}
 }
