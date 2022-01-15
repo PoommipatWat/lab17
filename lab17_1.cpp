ab0951c2bac2dfbbebd1944ab56b3ddaf3bece88
PoommipatWat <poommipat_wat@cmu.ac.th>
2022-01-16 02:47:23 +0700

    "lab17_1.cpp completed"

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -14,29 +14,23 @@ void findColSum(const double *,double *,int,int);
 
 int main(){
 	srand(time(0));
-	// const int N = 6, M = 8;
-	// double data[N][M] = {};
-	// double sum1[N] = {};
-	// double sum2[M] = {};
-	// double *dPtr = data[0];
-	// randData(dPtr,N,M);
-	// showData(dPtr,N,M);
-
-	// cout << "---------------------------------------------\n";
-
-	// findRowSum(dPtr,sum1,N,M);
-	// showData(sum1,N,1);
+	const int N = 6, M = 8;
+	double data[N][M] = {};
+	double sum1[N] = {};
+	double sum2[M] = {};
+	double *dPtr = data[0];
+	randData(dPtr,N,M);
+	showData(dPtr,N,M);
 	
-	// cout << "---------------------------------------------\n";
+	cout << "---------------------------------------------\n";
 	
-	// findColSum(dPtr,sum2,N,M); 
-	// showData(sum2,1,M);
+	findRowSum(dPtr,sum1,N,M);
+	showData(sum1,N,1);
 	
+	cout << "---------------------------------------------\n";
 	
-	double a[3][2] = {1,2,3,4,5,6};
-	double b[3];
-	findRowSum(a[0],b,3,2);
-	showData(b,3,1);
+	findColSum(dPtr,sum2,N,M); 
+	showData(sum2,1,M);
 }
 
 void showData(double *dPtr, int N, int M){
@@ -56,28 +50,20 @@ void randData(double *dPtr, int N, int M){
 
 void findRowSum(const double *dPtr,double *sum1,int N,int M){
 	int count = 0;
-	if (M == 1){
-		for (int i  = 0; i < N; i++){
-			sum1[i] = dPtr[i];
-		}
-	}
-	else{
-		for (int i  = 0; i < M*N; i++){
-			sum1[count] += dPtr[i];
-			if((i+1)%M == 0) count++;
-		}
+	double sum = 0;
+	for (int i  = 0; i < M*N; i++){
+		sum += dPtr[i];
+		if((i+1)%M == 0){
+		    sum1[count] = sum;
+		    sum = 0;
+		    count++;
+        }
 	}
 }
 
 void findColSum(const double *dPtr,double *sum1,int N,int M){
-	if(N == 1){
-		for (int i  = 0; i < M; i++){
-			sum1[i] = dPtr[i];
-		}
-	}
-	else{
-		for (int i  = 0; i < M*N; i++){
-			sum1[(i)%M] += dPtr[i];
-		}
+    for (int j = 0; j < M ; j++) sum1[j] = 0;
+	for (int i  = 0; i < M*N; i++){
+		sum1[(i)%M] += dPtr[i];
 	}
 }
